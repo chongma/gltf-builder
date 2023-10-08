339f8379e7ee90721751eb6d0affff54200b74ac
Matthew Broadhead <nbmlaw@gmail.com>
2023-10-08 18:07:19 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { GltfBuilder } from './builders/GltfBuilder'
 import { LinesBuilder } from './builders/LinesBuilder'
 import { PointsBuilder } from './builders/PointsBuilder'
 import { TrianglesBuilder } from './builders/TrianglesBuilder'
+import { Point, Gltf, LineType, TriangleType } from './types/gltf'
 
 export {
     GltfBuilder,
     LinesBuilder,
     PointsBuilder,
-    TrianglesBuilder
+    TrianglesBuilder,
+    Point,
+    Gltf,
+    LineType,
+    TriangleType
 }
