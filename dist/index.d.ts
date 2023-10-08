12cb3ce24b6400df77645092570772708348b3aa
Matthew Broadhead <nbmlaw@gmail.com>
2023-10-08 18:48:06 +0200

    Update distribution

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ import { GltfBuilder } from './builders/GltfBuilder';
 import { LinesBuilder } from './builders/LinesBuilder';
 import { PointsBuilder } from './builders/PointsBuilder';
 import { TrianglesBuilder } from './builders/TrianglesBuilder';
-export { GltfBuilder, LinesBuilder, PointsBuilder, TrianglesBuilder };
+import { Point, Gltf, LineType, TriangleType } from './types/gltf';
+export { GltfBuilder, LinesBuilder, PointsBuilder, TrianglesBuilder, Point, Gltf, LineType, TriangleType };
